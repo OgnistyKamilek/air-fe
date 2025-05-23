6f4a7ea8d2c09a4a702c98169c8335381c42ea2e
Szymon Olszak <szymon.olszak3214@gmail.com>
2025-05-23 02:03:10 +0200

    Beta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,12 @@ import {WeatherService} from './weather.service';
 import {AsyncPipe} from '@angular/common';
 import {Observable} from 'rxjs';
 import {HeaderComponent} from './header/header.component';
+import {SearchbarComponent} from './searchbar/searchbar.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, AsyncPipe, HeaderComponent],
+  imports: [RouterOutlet, RouterLink, AsyncPipe, HeaderComponent, SearchbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
