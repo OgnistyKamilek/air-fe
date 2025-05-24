76053c9659a5ee7bec9ddfd17c94f3afeaadf417
Szymon Olszak <szymon.olszak3214@gmail.com>
2025-05-24 22:42:11 +0200

    Map works

--- src/app/searchbar/searchbar.component.ts
+++ src/app/searchbar/searchbar.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-searchbar',
   imports: [],
+  standalone: true,
   templateUrl: './searchbar.component.html',
   styleUrl: './searchbar.component.css'
 })
