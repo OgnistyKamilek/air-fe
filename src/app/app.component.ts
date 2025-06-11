96ccd1f253cc4d8b30d757f03ba017c2b559b1c2
Kamil <kamil.bagrowski100@gmail.com>
2025-06-11 22:30:47 +0200

    Footer zrobiony

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,20 @@ import {Observable} from 'rxjs';
 import {HeaderComponent} from './header/header.component';
 import {SearchbarComponent} from './searchbar/searchbar.component';
 import {ApiMapComponent} from './api-map/api-map.component';
+import {FooterComponent} from './footer/footer.component';
+import { TermsOfServiceComponent } from './terms-of-service/terms-of-service.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, AsyncPipe, HeaderComponent, SearchbarComponent, ApiMapComponent],
+  imports: [RouterOutlet, RouterLink, AsyncPipe, HeaderComponent, SearchbarComponent, ApiMapComponent, FooterComponent, TermsOfServiceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
+
   // title = 'air-fe';
   // private weatherService = inject(WeatherService);
   // stations$: Observable<any> = this.weatherService.getStations()
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MeasurementHistoryComponent } from './measurement-history/measurement-history.component';
+import {TermsOfServiceComponent} from './terms-of-service/terms-of-service.component';
+import {PrivacyPolicyComponent} from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +12,15 @@ export const routes: Routes = [
   {
     path: 'history',
     component: MeasurementHistoryComponent
+  },
+  {
+    path: 'terms-of-service',
+    component: TermsOfServiceComponent
+
+  },
+  {
+    path: 'privacy-policy',
+    component: PrivacyPolicyComponent
+
   }
 ];
